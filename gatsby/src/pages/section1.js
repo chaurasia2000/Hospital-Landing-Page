8e06e2b25707f20dddb489b4289021570118a6a9
Chaurasia2000 <krishnachaurasia2425@gmail.com>
2024-06-02 09:30:57 +0530

    one change svg to div

--- gatsby/src/pages/section1.js
+++ gatsby/src/pages/section1.js
@@ -17,7 +17,7 @@ const Section1 = () => {
         </div>
         </div>
         <div className="col-auto d-none d-lg-block" id="fsvg">
-          <svg className="bd-placeholder-img rounded-4" width="470" height="672" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title></title><rect id="rect1" width="100%" height="100%" fill="#EBEEFF"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em"></text></svg>
+          <div className="rounded-4" id="sec1divp2"></div>
         </div>
       </div>
     </div>
